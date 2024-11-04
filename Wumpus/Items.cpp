f43b53e696180acdecbd5cd79634ce645ecf2f62
Leigh <makenna.goetsch@gmail.com>
2024-11-03 21:32:58 -0600

    fill out items class

---
+++ Wumpus/Items.cpp
@@ -0,0 +1,62 @@
+/*
+ *
+ *
+ */
+
+#include "Items.h"
+#include "Utils.h"
+
+
+#include <iostream>
+
+#include "Room.h"
+
+void AirTank::pickup(Player player) {
+  player.addAir(air);
+  std::cout << "AirTank::pickup" << std::endl << "Current Air: " << player.getAir() << std::endl;
+}
+
+void Net::pickup(Player player) {
+  player.addNet();
+}
+
+bool Net::use(Player player, char direction) {
+  switch (direction) {
+    case "n":
+      Room northRoom = player.getRoom()->getNorth();
+      return northRoom.hasKraken() || northRoom.getEast().hasKraken() || northRoom.getWest().hasKraken();
+      break;
+    case "e":
+      Room eastRoom = player.getRoom()->getEast();
+      return eastRoom.hasKraken() || eastRoom.getNorth().hasKraken() || eastRoom.getSouth().hasKraken();
+    case "s":
+      Room southRoom = player.getRoom()->getSouth();
+      return southRoom.hasKraken() || southRoom.getEast().hasKraken() || southRoom.getWest().hasKraken();
+    case "w":
+      Room westRoom = player.getRoom()->getWest();
+      return westRoom.hasKraken() || westRoom.getNorth().hasKraken() || westRoom.getSouth().hasKraken();
+    default:
+      throw std::invalid_argument("Invalid direction");
+      return false;
+  }
+}
+
+void Harpoon::pickup(Player player) {
+  player.addHarpoon();
+}
+
+bool Harpoon::use(Player player, char direction) {
+  switch (direction) {
+    case "n":
+      return player.getRoom()->getNorth().hasKraken();
+    case "e":
+      return player.getRoom()->getEast().hasKraken();
+    case "s":
+      return player.getRoom()->getSouth().hasKraken();
+    case "w":
+      return player.getRoom()->getWest().hasKraken();
+    default:
+      throw std::invalid_argument("Invalid direction");
+      return false;
+  }
+}
---
+++ Wumpus/Items.h
@@ -0,0 +1,46 @@
+/*
+ *
+ *
+ */
+
+#ifndef ITEM_H
+#define ITEM_H
+
+#include "Player.h"
+
+class Items {
+public:
+  virtual ~Items() = default;
+
+  virtual void pickup(Player player);
+
+private:
+  bool used = false;
+};
+
+class AirTank : public Items {
+public:
+  AirTank(int air) : air(air) {}
+  void pickup(Player player) override;
+
+private:
+  int air;
+};
+
+class Weapon : public Items {
+public:
+  virtual bool use(Player player, char direction);
+};
+
+class Net : public Weapon {
+public:
+  void pickup(Player player) override;
+  bool use(Player player, char direction) override;
+};
+
+class Harpoon : public Weapon {
+public:
+  void pickup(Player player) override;
+  bool use(Player player, char direction) override;
+};
+#endif //ITEM_H
