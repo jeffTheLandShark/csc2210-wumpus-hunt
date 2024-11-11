410e81ce97c0a387d1f64417e4e432a272a6cdd1
deald262 <deald@msoe.edu>
2024-11-10 23:30:04 -0600

    changed source folder to src

---
+++ src/room.h
@@ -0,0 +1,38 @@
+//
+// Created by deald on 10/27/2024.
+//
+
+#ifndef ROOM_H
+#define ROOM_H
+#include "player.h"
+#include "innards.h"
+
+class Room {
+public:
+  Room();
+  void setNorth(Room *room);
+  void setSouth(Room *room);
+  void setEast(Room *room);
+  void setWest(Room *room);
+
+  Room *getNorth() const;
+  Room *getSouth() const;
+  Room *getEast() const;
+  Room *getWest() const;
+
+  bool hasKraken() const;
+  void setInnard(Innards *innard);
+  Innards *getInnard() const;
+  void display() const;
+  bool isEdge() const;
+
+private:
+  Room *north;
+  Room *east;
+  Room *south;
+  Room *west;
+
+  Innards *innard;
+};
+
+#endif //ROOM_H
